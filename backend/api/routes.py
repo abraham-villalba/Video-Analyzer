b253d6448bba3aed15977ec70a252011206e2ba9
abraham-villalba <abraham.villalba@encora.com>
2025-04-07 13:28:11 -0700

    Added and configured first version of logger

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from api.utils.schemas import AnalyzeVideoRequestSchema, validate_analyze_video_request
 from api.utils.response_model import ResponseModel
-
+from api.utils.logger import logger
 
 # Create a Blueprint object to define the routes
 api_bp = Blueprint('api', __name__) 
@@ -10,8 +10,10 @@ api_bp = Blueprint('api', __name__)
 def analyze_video():
     """ Handles Video Analyzis endpoint """
     data, errors = validate_analyze_video_request(request.get_json(), AnalyzeVideoRequestSchema())
-
+    logger.info(f"Analyzing video...")
+    logger.debug(f"Analyzing video with data : {data}")
     if errors:
+        logger.error(f"Error analyzing video: {errors}")
         return ResponseModel(status="error", error=errors).to_json(), 400 # Bad Request
 
     print(f"Request: {data}")
---
+++ backend/api/utils/logger.py
@@ -0,0 +1,32 @@
+"""
+"""
+
+import logging
+import os
+
+# Make sure logs directory exits
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Logger configuration
+logger = logging.getLogger("video_analyzer")
+logger.setLevel(logging.DEBUG)  # Use INFO or WARNING in production
+
+# File handler
+file_handler = logging.FileHandler(f"{LOG_DIR}/app.log")
+file_handler.setLevel(logging.DEBUG)
+
+# Console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
