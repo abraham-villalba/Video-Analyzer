931823cf3629f41215acb5db3b2cbc778f6b4590
abraham-villalba <abraham.villalba@encora.com>
2025-04-08 13:38:22 -0700

    Added first version of endpoint to upload a video for later analysis

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -2,9 +2,25 @@ from flask import Blueprint, request
 from api.utils.schemas import AnalyzeVideoRequestSchema, validate_analyze_video_request
 from api.utils.response_model import ResponseModel
 from api.utils.logger import logger
+from api.utils.video_utils import store_video
+
+
 
 # Create a Blueprint object to define the routes
 api_bp = Blueprint('api', __name__) 
+
+@api_bp.route('/upload_video', methods=['POST'])
+def upload_video():
+    """ Endpoint to submit a video for later processing """
+    if 'video' not in request.files:
+        logger.error("Error uploading video, file is missing")
+        return ResponseModel(status="error", error="Error uploading video, file is missing").to_json(), 400
+
+    video = request.files['video']
+    print(video)
+    video_id = store_video(video)
+
+    return ResponseModel(status='success', data={'id': video_id}).to_json(), 200
 
 @api_bp.route('/analyze_video', methods=['POST'])
 def analyze_video():
--- backend/api/utils/video_utils.py
+++ backend/api/utils/video_utils.py
@@ -2,6 +2,13 @@
 import subprocess
 import os
 from api.utils.logger import logger
+from werkzeug.utils import secure_filename
+from uuid import uuid4
+from dotenv import load_dotenv
+
+load_dotenv()
+
+UPLOAD_DIR = os.getenv('UPLOAD_DIR', 'uploads')
 
 def extract_audio(video_path : str, audio_path : str) -> None:
     """
@@ -29,3 +36,25 @@ def extract_audio(video_path : str, audio_path : str) -> None:
     except subprocess.CalledProcessError as e:
         logger.error(f"Error during audio extraction: {str(e)}")
         raise e
+    
+def store_video(video_file):
+    """
+    Generates an id for a video file and stores it on the system with its corresponding audio file
+    :param video_file: The video to store
+    :returns id identifier for this video analysis storage
+    """
+    filename = secure_filename(video_file.filename)
+    directory_id = str(uuid4())
+    save_dir = os.path.join(UPLOAD_DIR, directory_id)
+    logger.info(f"Storing video with (name, id): ({filename}, {directory_id})")
+
+    os.makedirs(save_dir, exist_ok=True)
+
+    video_path = os.path.join(save_dir, filename)
+    video_file.save(video_path)
+
+    # Extract audio
+    audio_path = os.path.join(save_dir, "audio.wav")
+    extract_audio(video_path, audio_path)
+
+    return directory_id
